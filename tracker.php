384ab63f5df19f6e71f3b24bc0205b7281252710
bridge-four <12418879+bridge-four@users.noreply.github.com>
2020-04-30 11:02:46 -0400

    modify with more data collection

--- tracker.php
+++ tracker.php
@@ -1,14 +1,24 @@
 <?php
 
+$delimiter = "\t";
+
 //THIS RETURNS THE IMAGE
 header('Content-Type: image/gif');
 readfile('tracking.gif');
 
 
-//THIS IS THE SCRIPT FOR THE ACTUAL TRACKING
-$date = date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
-$txt = $date.",". $_SERVER['REMOTE_ADDR'];
-$myfile = file_put_contents('log.txt', $txt.PHP_EOL , FILE_APPEND);
+//Collect data
+$data = [
+    'time'      => date('m/d/Y H:i:s'),
+    'url'       => "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
+    'ip'        => $_SERVER['REMOTE_ADDR'],
+    'referer'   => $_SERVER['HTTP_REFERER'],
+    'useragent' => $_SERVER['HTTP_USER_AGENT'],
+    'browser'   => get_browser(null, true),
+];
+
+//Save data to file
+file_put_contents('log.txt', implode($delimiter, array_values($data)).PHP_EOL , FILE_APPEND | LOCK_EX);
 exit;
 
 ?>
